50d6a922844ad67d98857365ce9a6e0b485c4004
Abdul_Malik <malik.info28@gmail.com>
2025-04-22 02:43:49 +0530

    enhanced form by saving email in localstorage and sets it when it reloads

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -52,9 +52,9 @@
 //   }
 // }
 
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { of } from 'rxjs';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -79,7 +79,8 @@ function emailIsUnique(control: AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.email, Validators.required],
@@ -95,6 +96,27 @@ export class LoginComponent {
   }
   get IsPasswordInvalid() {
     return (this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid);
+  }
+
+  ngOnInit() {
+    const savedForm = window.localStorage.getItem('saved-login-form');
+
+    if (savedForm) {
+      const loadedForm = JSON.parse(savedForm);
+      // this.form.setValue({ email: loadedForm.email, password: '' });
+      // this.form.controls.email.setValue(loadedForm.email);
+      this.form.patchValue({
+        email: loadedForm.email
+      });
+    }
+
+    const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: value => {
+        window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }))
+      }
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe())
   }
 
   onSubmit() {
